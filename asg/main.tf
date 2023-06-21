a85abd98d369c902e62a5bff46013bd25b88da47
Yavuz Ayguner <128009269+yavuzzi@users.noreply.github.com>
2023-06-20 20:12:25 -0500

    added prods

---
+++ asg/main.tf
@@ -0,0 +1,25 @@
+
+
+module "asg" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+
+  # Autoscaling group
+  name = "example-asg"
+
+  min_size                  = 3
+  max_size                  = 99
+  desired_capacity          = 3
+  wait_for_capacity_timeout = 0
+  health_check_type         = "EC2"
+  vpc_zone_identifier       = data.terraform_remote_state.vpc.outputs.public_subnets
+
+  # Launch template
+  launch_template_name        = "example-asg"
+  launch_template_description = "Launch template example"
+  update_default_version      = true
+
+  image_id          = data.aws_ami.ubuntu.id
+  instance_type     = "t3.micro"
+  ebs_optimized     = true
+  enable_monitoring = true
+}
